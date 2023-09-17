c94f859d8cf6850831729c3016544199b91f05d6
Sergei Sergienko <divelix1@gmail.com>
2023-09-17 19:53:40 +0300

    feat: Added pyproject.toml

---
+++ __main__.py
@@ -0,0 +1,17 @@
+import sys
+import tomllib
+
+from PyQt5.QtWidgets import QApplication
+
+from src import MainWindow
+
+
+if __name__ == "__main__":
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
+    path_to_dataset = config["dataset"]["path"]
+    app = QApplication(sys.argv)
+    mw = MainWindow(path_to_dataset)
+    mw.show()
+    mw.load_latest_sample()
+    sys.exit(app.exec_())
